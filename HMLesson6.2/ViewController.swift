a536385cf33425454b9d34320c4c694bc46e7973
Vlad <lokotuin12@mail.ru>
2025-08-19 16:54:47 +0300

    Initial Commit

---
+++ HMLesson6.2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HMLesson6.2
+//
+//  Created by Игнат Гончаров on 19.08.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
